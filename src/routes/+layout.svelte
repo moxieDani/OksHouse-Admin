73e66c0cfcbfaf57b169c098246de81fb8cca2dc
Kisoon Kwon <moxie2ks@gmail.com>
2025-08-23 15:05:26 +0900

    PWA 설치 기능 제거

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,5 @@
 <script>
 	import '../app.css';
-	import { browser } from '$app/environment';
-	import { onMount } from 'svelte';
-	onMount(() => {
-		if (browser) {
-			// PWA 설치 프롬프트 이벤트 리스너
-			window.addEventListener('beforeinstallprompt', (e) => {
-				e.preventDefault();
-				deferredPrompt = e;
-			});
-
-			// PWA 설치 완료 이벤트
-			window.addEventListener('appinstalled', () => {
-				// 설치 완료시에는 로그 출력하지 않음
-			});
-		}
-	});
-
 </script>
 
 <main class="container">
